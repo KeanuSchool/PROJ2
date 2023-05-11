6d081183c5e545a88780de9b2c81f3e588800c6d
KeanuSchool <22118918@student.hhs.nl>
2023-05-11 10:24:56 +0200

    new

--- proj2/src/main/java/com/example/proj2/HelloController.java
+++ proj2/src/main/java/com/example/proj2/HelloController.java
@@ -1,11 +1,21 @@
 package com.example.proj2;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button forgotPasswordButton;
+
+    @FXML
+    private Button createAccountButton;
+
 
     @FXML
     protected void onHelloButtonClick() {
