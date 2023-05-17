81547b20037c345b0abe12ddc7456d4a81ce6f6b
KeanuSchool <22118918@student.hhs.nl>
2023-05-17 15:37:31 +0200

    newnew

--- proj2/src/main/java/com/example/proj2/HelloController.java
+++ proj2/src/main/java/com/example/proj2/HelloController.java
@@ -1,8 +1,16 @@
 package com.example.proj2;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
@@ -21,4 +29,25 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    @FXML
+    public void switchToScene2(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("scene2.fxml"));
+        Parent scene2Root = fxmlLoader.load();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(scene2Root, 320, 240));
+        stage.setTitle("AIsistify");
+        stage.show();
+    }
+    @FXML
+    public void switchToScene3(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("scene3.fxml"));
+        Parent scene2Root = fxmlLoader.load();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(scene2Root, 320, 240));
+        stage.setTitle("AIsistify");
+        stage.show();
+    }
+
+
 }
